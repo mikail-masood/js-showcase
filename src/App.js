bb2ccb937459322f7c14c5144e2337669c686ac9
Mikail <mikail.masood@mail.utoronto.ca>
2020-11-27 22:31:43 -0500

    added connections to nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import FloatingNav from './components/FloatingNav';
 
 function App() {
   return (
     <div className="App">
       <h1>App Work?</h1>
+      <FloatingNav/>
     </div>
   );
 }
---
+++ src/components/FloatingNav.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class FloatingNav extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This will be the floating nav button</h1>
+      </div>
+    )
+  }
+}
+
+export default FloatingNav
