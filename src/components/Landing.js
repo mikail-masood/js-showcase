4a99446ba49cbb4f9178b95b57b613ac44ec2466
Mikail <mikail.masood@mail.utoronto.ca>
2020-11-28 17:49:33 -0500

    full page background component

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Landing.css';
+
+export class Landing extends Component {
+  render() {
+    return (
+      <div className='landing_background'>
+      </div>
+    )
+  }
+}
+
+export default Landing
