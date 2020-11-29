801a84395f27d38b2500da4efb3ab481bf1940f2
Mikail <mikail.masood@mail.utoronto.ca>
2020-11-28 20:50:18 -0500

    added color variables to css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import FloatingNav from './components/FloatingNav';
 import Landing from './components/Landing';
+import SideNav from './components/SideNav';
 
 function App() {
   return (
     <div className="App">
       <Landing/>
       <FloatingNav/>
-      
+      <SideNav/>
     </div>
   );
 }
