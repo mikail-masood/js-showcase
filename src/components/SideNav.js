d672ac3991631f53edb794feadaeb1580bb3e724
Mikail <mikail.masood@mail.utoronto.ca>
2020-11-28 20:49:13 -0500

    start sidenav bar

---
+++ src/components/SideNav.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './SideNav.css';
+
+export class SideNav extends Component {
+  render() {
+    return (
+      <div className='sidenav-container'>
+        <h1> item</h1>
+        <h1> item</h1>
+        <h1> item</h1>
+      </div>
+    )
+  }
+}
+
+export default SideNav
