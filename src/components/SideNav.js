a40164c1bd5ea8faa9a4bfe6fec49a11da2ef985
Mikail <mikail.masood@mail.utoronto.ca>
2020-12-02 08:27:47 -0500

    new sidebar custom component

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './SideNav.css';
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
 
 export class SideNav extends Component {
   render() {
     return (
-      <div className='sidenav-container'>
-        <h1> item</h1>
-        <h1> item</h1>
-        <h1> item</h1>
-      </div>
+      <ProSidebar>
+      <Menu iconShape="square">
+        <MenuItem>Dashboard</MenuItem>
+          <SubMenu title="Components">
+            <MenuItem>Component 1</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+          </SubMenu>
+      </Menu>
+      </ProSidebar>
     )
   }
 }
